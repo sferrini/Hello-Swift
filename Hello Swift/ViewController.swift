dfd015aa484034047d8aef54850822afe3ca0303
Simone Ferrini <sferrini@hotmail.it>
2014-06-03 00:23:05 +0100

    Added UILabel, @IBOutlet and text

--- Hello Swift/ViewController.swift
+++ Hello Swift/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
                             
+    @IBOutlet var helloLabel : UILabel = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        helloLabel.text = "Hello Swift"
     }
 
     override func didReceiveMemoryWarning() {
