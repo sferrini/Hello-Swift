07c074c2821212067cccba9df6dab58d9c2b09ab
Simone Ferrini <sferrini@hotmail.it>
2014-06-03 00:10:56 +0100

    Initial Commit

---
+++ Hello Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello Swift
+//
+//  Created by Simone Ferrini on 03/06/14.
+//  Copyright (c) 2014 sferrini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
